9d6061ce54a01e5d22c1e95b7b7266e8839ae837
AkilaDemotte <akilamilindademotte@gmail.com>
2024-05-27 15:54:18 +0530

    Inital commit for backend

---
+++ src/main/java/com/example/employeems/dto/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package com.example.employeems.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EmployeeDTO {
+    private int empID;
+    private String name;
+    private String empAddress;
+    private String empMNumber;
+}
